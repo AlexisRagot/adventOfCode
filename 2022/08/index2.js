adff46e2be34f2259e97ebd5a35ec5c5be192c3d
Alexis Ragot <a.ragot.alexis@gmail.com>
2022-12-19 11:31:42 +0100

    aoc day 8 part 2

--- 2022/08/index2.js
+++ 2022/08/index2.js
@@ -2,11 +2,74 @@ const { inputs } = require("./input");
 
 // part 2
 const run = (inputs) => {
-    let nbVisible = 0
+    const candidates = []
 
-    return nbVisible
+    for (let i = 0; i < inputs.length; i++) {
+        for (let j = 0; j < inputs[i].length; j++) {
+            if (i === 0 || j === 0 || i === (inputs.length -1) || j === (inputs[i].length -1)) {
+                continue
+            }
+
+            const sizeFromTop = getTopDistanceOfLargeTree(inputs, i, j)
+            const sizeFromRight = getRightDistanceOfLargeTree(inputs, i, j)
+            const sizeFromRBottom = getBottomDistanceOfLargeTree(inputs, i, j)
+            const sizeFromLeft = getLeftDistanceOfLargeTree(inputs, i, j)
+
+            candidates.push(sizeFromTop * sizeFromRight * sizeFromRBottom * sizeFromLeft)
+        }
+    }
+
+    return candidates.sort((a, b) => a-b).pop()
 }
 
+const getTopDistanceOfLargeTree = (inputs, i, j) => {
+    const treeSize = inputs[i][j]
+    let distance = 1
+
+    for (let verticalPos = i-1; verticalPos > 0; verticalPos--, distance++) {
+        if (inputs[verticalPos][j] >= treeSize) {
+            return distance
+        }
+    }
+
+    return distance
+}
+const getBottomDistanceOfLargeTree = (inputs, i, j) => {
+    const treeSize = inputs[i][j]
+    let distance = 1
+
+    for (let verticalPos = i+1; verticalPos < inputs.length-1; verticalPos++, distance++) {
+        if (inputs[verticalPos][j] >= treeSize) {
+            return distance
+        }
+    }
+
+    return distance
+}
+const getLeftDistanceOfLargeTree = (inputs, i, j) => {
+    const treeSize = inputs[i][j]
+    let distance = 1
+
+    for (let horizontalPos = j-1; horizontalPos > 0; horizontalPos--, distance++) {
+        if (inputs[i][horizontalPos] >= treeSize) {
+            return distance
+        }
+    }
+
+    return distance
+}
+const getRightDistanceOfLargeTree = (inputs, i, j) => {
+    const treeSize = inputs[i][j]
+    let distance = 1
+
+    for (let horizontalPos = j+1; horizontalPos < inputs[i].length-1; horizontalPos++, distance++) {
+        if (inputs[i][horizontalPos] >= treeSize) {
+            return distance
+        }
+    }
+
+    return distance
+}
 
 console.log('result : ',  run(inputs));
 
--- 2022/08/index2.test.js
+++ 2022/08/index2.test.js
@@ -4,6 +4,12 @@ var assert = require('assert');
 
 describe('run', function() {
     it('small array', function () {
-
+        assert.equal(instance.run([
+            [3, 0, 3, 7, 3],
+            [2, 5, 5, 1, 2],
+            [6, 5, 3, 3, 2],
+            [3, 3, 5, 4, 9],
+            [3, 5, 3, 9, 0],
+        ]), 8);
     });
 });
